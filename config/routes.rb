255ef4991c9a8c907ef1325cbb654f0015149031
Omar Mendoza <mendoza.omar.85@gmail.com>
2016-03-28 22:21:23 -0700

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index]
-    resources :sponsored_posts, except: [:index, :update, :destroy, :create]
+    resources :sponsored_posts, only: [:new, :edit, :show]
   end
 
 
