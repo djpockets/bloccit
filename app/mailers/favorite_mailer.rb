d1c1e44b0fd8b732f2188ac5c4b34a3141355c9f
Omar Mendoza <mendoza.omar.85@gmail.com>
2016-04-10 22:22:40 -0700

    Assignment 44: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,4 +15,14 @@ class FavoriteMailer < ApplicationMailer
     mail(to: "conradwt@gmail.com", subject: "New Comment on #{post.title}")
     mail(cc: "mendoza.omar.85@gmail.com", subject: "Test Email")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@bloccit.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
+    headers["References"] = "<post/#{post.id}@bloccit.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "New post #{post.title}!")
+  end
 end
