fe8bf18b82212810c0c99fe76b8ca293e605f5be
Omar Mendoza <mendoza.omar.85@gmail.com>
2016-04-15 00:49:14 -0700

    Assignment 47: stuck on vote ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,22 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
-    redirect_to :back
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def down_vote
     update_vote(-1)
-    redirect_to :back
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   private
@@ -21,5 +31,6 @@ class VotesController < ApplicationController
     else
       @vote = current_user.votes.create(value: new_value, post: @post)
     end
+
   end
 end
